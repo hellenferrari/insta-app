a49430fe162eab92440cc54b9167a2f56e6a55db
Hellen Ferrari <hellenc.ferrari@gmail.com>
2022-08-28 16:57:15 -0700

    Create Authorization feature project and add to the App

---
+++ Authorization/Authorization/Authorization.h
@@ -0,0 +1,15 @@
+//	
+// Copyright © Hellen Ferrari. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Authorization.
+FOUNDATION_EXPORT double AuthorizationVersionNumber;
+
+//! Project version string for Authorization.
+FOUNDATION_EXPORT const unsigned char AuthorizationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Authorization/PublicHeader.h>
+
+
--- EssentialApp/EssentialApp/ViewController.swift
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -4,6 +4,7 @@
 
 import UIKit
 import EssentialFeed
+import Authorization
 
 class ViewController: UIViewController {
 
