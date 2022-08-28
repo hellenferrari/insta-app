850bed873ec95a2cbb68b105fb6d818e9c4324c5
Hellen Ferrari <hellenc.ferrari@gmail.com>
2022-08-28 16:58:17 -0700

    Create Message feature project and add to the App

---
+++ Messaging/Messaging/Messaging.h
@@ -0,0 +1,15 @@
+//	
+// Copyright © Hellen Ferrari. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Messaging.
+FOUNDATION_EXPORT double MessagingVersionNumber;
+
+//! Project version string for Messaging.
+FOUNDATION_EXPORT const unsigned char MessagingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Messaging/PublicHeader.h>
+
+
